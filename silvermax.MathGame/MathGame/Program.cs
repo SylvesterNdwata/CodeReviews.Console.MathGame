a5237bb9b4cf77f4f78fc16c9431a77117c2baab
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-04-21 09:14:36 +0200

    Fix game loop issues and improved UX based the feedback

--- silvermax.MathGame/MathGame/Program.cs
+++ silvermax.MathGame/MathGame/Program.cs
@@ -16,12 +16,14 @@ var points = 0;
 
 Random rand = new Random();
 
-List<string> completedQuestions = new();
+List<string> correctQuestions = new();
+List<string> wrongQuestions = new();
 
-Stopwatch sw = Stopwatch.StartNew();
+//Stopwatch sw = Stopwatch.StartNew();
 
+bool gameContinue = true;
 
-for (int i = 0; i <= questionNumber; i++)
+while (gameContinue)
 {
     Console.WriteLine(@"Please Select an Operation:
     1. Addition(+)
@@ -29,9 +31,11 @@ for (int i = 0; i <= questionNumber; i++)
     3. Multiplication(x)
     4. Division(/)
     5. Choose this option to see how many points you have.
-    6. Choose this option to see the questions that were done");
+    6. Choose this option to see the questions that were done.
+    7. Choose the option to exit the game.");
 
     readResult = Console.ReadLine();
+    Console.Clear();
 
     if (readResult != null)
     {
@@ -39,32 +43,40 @@ for (int i = 0; i <= questionNumber; i++)
         {
             case "1":
 
-                Game(Addition, "+");
+                Game(Addition, "+", questionNumber);
                 break;
 
             case "2":
 
-                Game(Subtraction, "-");
+                Game(Subtraction, "-", questionNumber);
                 break;
 
             case "3":
-                Game(Multiplication, "x");
+                Game(Multiplication, "x", questionNumber);
                 break;
 
             case "4":
-                Game(Division, "/");
+                Game(Division, "/", questionNumber);
                 break;
 
             case "5":
                 Console.WriteLine($"You have {points} points");
+                Console.WriteLine("\nPress Enter to continue...");
+                Console.ReadLine();
+                Console.Clear();
                 break;
 
             case "6":
-                foreach (string qn in completedQuestions)
-                {
-                    Console.WriteLine(qn);
-                }
+                printQuestionHistory();
+                Console.WriteLine("\nPress Enter to continue...");
+                Console.ReadLine();
+                Console.Clear();
                 break;
+
+            case "7":
+                gameContinue = false;
+                break;
+
             default:
                 Console.WriteLine("Please enter a valid choice!");
                 break;
@@ -72,52 +84,83 @@ for (int i = 0; i <= questionNumber; i++)
     }
 }
 
-Console.WriteLine("Here are the questions you answered:");
-foreach (string qn in completedQuestions)
+//Console.WriteLine("Here are the questions you answered:");
+//foreach (string qn in completedQuestions)
+//{
+//    Console.WriteLine(qn);
+//}
+
+//sw.Stop();
+//Console.WriteLine($"You took {sw.Elapsed.TotalMinutes} minutes to finish the game");
+
+void Game(Func<int, int, int> operation, string opSign, int repitition)
 {
-    Console.WriteLine(qn);
-}
+    Stopwatch sw = Stopwatch.StartNew();
 
-sw.Stop();
-Console.WriteLine($"You took {sw.Elapsed.TotalMinutes} minutes to finish the game");
-
-void Game(Func<int, int, int> operation, string opSign)
-{
-    var num1 = rand.Next(1, 101);
-    var num2 = rand.Next(1, 101);
-
-    if (opSign.Equals("/"))
+    for (int i = 0; i <= repitition + 1; i++)
     {
-        do
+        var num1 = rand.Next(1, 101);
+        var num2 = rand.Next(1, 101);
+
+        if (opSign.Equals("/"))
         {
-            num1 = rand.Next(1, 101);
-            num2 = rand.Next(1, 101);
-        } while (num1 % num2 != 0);
-    }
-
-    var result = operation(num1, num2);
-
-    Console.WriteLine($"What is {num1} {opSign} {num2}?");
-    var readResult = Console.ReadLine();
-
-    if (readResult != null)
-    {
-        if (!int.TryParse(readResult, out int answer))
-        {
-            Console.WriteLine("Please input a valid integer answer");
-            return;
+            do
+            {
+                num1 = rand.Next(1, 101);
+                num2 = rand.Next(1, 101);
+            } while (num1 % num2 != 0);
         }
 
-        if (answer == result)
+        var result = operation(num1, num2);
+
+        Console.WriteLine($"What is {num1} {opSign} {num2}?");
+        var readResult = Console.ReadLine();
+
+        if (readResult != null)
         {
-            points++;
-        } else
-        {
-            Console.WriteLine($"You got the question wrong, the answer is {result}");
+            if (!int.TryParse(readResult, out int answer))
+            {
+                Console.WriteLine("Please input a valid integer answer");
+                return;
+            }
+
+            if (answer == result)
+            {
+                points++;
+                correctQuestions.Add($"{num1} {opSign} {num2} = {result}");
+            }
+            else
+            {
+                Console.WriteLine($"You got the question wrong, the answer is {result}");
+                wrongQuestions.Add($"{num1} {opSign} {num2} = {answer}. The correct answer was {result}");
+
+            }
         }
+
+        
+        Console.Clear();
     }
 
-    completedQuestions.Add($"{num1} {opSign} {num2} = {result}");
+    //Console.WriteLine("Here are the questions you answered correctly:");
+    //foreach (string qn in correctQuestions)
+    //{
+    //    Console.WriteLine(qn);
+    //}
+    //Console.WriteLine();
+
+    //Console.WriteLine("Here are the questions you answered incorrectly:");
+    //foreach (string qn in wrongQuestions)
+    //{
+    //    Console.WriteLine(qn);
+    //}
+    //Console.WriteLine();
+    printQuestionHistory();
+
+    sw.Stop();
+    Console.WriteLine($"You took {sw.Elapsed.TotalMinutes} minutes to finish the game");
+    Console.WriteLine("\nPress Enter to continue...");
+    Console.ReadLine();
+    Console.Clear();
 }
 
 int Addition(int num1, int num2) => num1 + num2;
@@ -127,3 +170,20 @@ int Subtraction(int num1, int num2) => num1 - num2;
 int Multiplication(int num1, int num2) => num1 * num2;
 
 int Division(int num1, int num2) => num1 / num2;
+
+void printQuestionHistory()
+{
+    Console.WriteLine("Here are the questions you answered correctly:");
+    foreach (string qn in correctQuestions)
+    {
+        Console.WriteLine(qn);
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Here are the questions you answered incorrectly:");
+    foreach (string qn in wrongQuestions)
+    {
+        Console.WriteLine(qn);
+    }
+    Console.WriteLine();
+}
